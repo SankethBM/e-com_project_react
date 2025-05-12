0d73adeb3c9ffdf3fd77c40c3a8be988a2a73bfc
Sanketh B M <sankethbm333@gmail.com>
2025-05-12 22:12:04 +0530

    api slice done for home screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,8 @@ import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import { Provider } from "react-redux";
+import store from "./store";
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -26,7 +28,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
