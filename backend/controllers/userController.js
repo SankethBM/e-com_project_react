dbc0926c7d25b56fc2c4dec4049c8e5dfde61d09
Sanketh B M <sankethbm333@gmail.com>
2025-05-14 12:58:07 +0530

    completed with payment method in frontend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -61,14 +61,10 @@ const registerUser = asyncHandler(async (req, res) => {
 // @desc    Logout user / clear cookie
 // @route   GET /api/user/logout
 // @access  Public
-const logoutUser = asyncHandler(async (req, res) => {
-  res.cookie("jwt", "", {
-    httpOnly: true,
-    expires: new Date(0),
-  });
-
-  res.status(200).json({ message: "Logged Out Succesfully !" });
-});
+const logoutUser =  (req, res) => {
+  res.clearCookie('jwt');
+  res.status(200).json({ message: 'Logged out successfully' });
+};
 
 // @desc    Get user profile
 // @route   GET /api/user/profile
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,13 +12,15 @@ import { Provider } from "react-redux";
 import store from "./store";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PrivateRoute from "./components/PrivateRoute";
+import PaymentScreen from "./screens/PaymentScreen";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
-
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -28,6 +30,12 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
+
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+        <Route path="/payment" element={<PaymentScreen />} />
+
+      </Route>
     </Route>
   )
 );
